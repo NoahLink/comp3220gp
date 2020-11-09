a1c3e7ab8a3832b6966fd361c9f77be5682b3da7
NoahLink <71844852+NoahLink@users.noreply.github.com>
2020-11-09 16:20:30 -0500

    Add files via upload

--- DataResource.java
+++ DataResource.java
@@ -15,20 +15,23 @@ public class DataResource {
 	String dir, dirName;
 	List<File> fileList;
 	File file;
+	Boolean created = false;
 
-	DataResource() {
-		Scanner sc  = new Scanner(System.in);
-		int i = 0;
-		createDirectory();
-		while (i!=2){
-			addFile();
-			
-			// Add another or go back to main menu
-			System.out.println("Press (1) to add another file. Press (2) to return to Main Menu.");
-			i = sc.nextInt();
-		}
+// Do nothing but declare on creation	
+//	DataResource() {
+//		Scanner sc  = new Scanner(System.in);
+//		int i = 0;
+//		createDirectory();
+//		while (i!=2){
+//			addFile();
+//			
+//			// Add another or go back to main menu
+//			System.out.println("Press (1) to add another file. Press (2) to return to Main Menu.");
+//			i = sc.nextInt();
+//		}
+//
+//	}
 
-	}
 
 	public void createDirectory() {
 		Scanner sc = new Scanner(System.in);
@@ -38,39 +41,95 @@ public class DataResource {
 		String dir = "";
 		boolean b = false;
 		while (!b) {
-			System.out.println("Enter the name of the Data Resource you would like to create: ");
+			System.out.println("Enter the name of the Data Resource Directory you would like to create: ");
 			dirName = sc.nextLine();
-			System.out.println("Enter the directory you'd like it saved in: ");
+			System.out.println("Enter the location you'd like it saved in: ");
 			dir = sc.nextLine();
 			dir = dir + dirName;
 			File f = new File(dir);
 			b = f.mkdir();
 			if (!b)
-				System.out.println("Unable to create a Data Resource with this name. Try another: ");
+				System.out.println("Unable to create a Data Resource with this name or location. Try another: ");
 		}
 
-		System.out.println("Data Resource successfully created!");
+		System.out.println("Data Resource Directory successfully created!");
 		// save directory/name
 		this.dirName = dirName;
 		this.dir = dir;
+		this.created = true;
 
 	}
 
 	public void addFile(){
+		//if there is not directory
+		if(this.created == false) {
+			this.createDirectory();
+		}
+		
 		// Get file to add to Data Resource
 		Scanner sc = new Scanner(System.in);
 		String filepath;
-		System.out.println("Enter the pathname of the file you would like to add to your Data Resource: ");
+		System.out.println("Enter the path of the file you would like to add to your Data Resource: ");
 		filepath = sc.nextLine();
 		File f = new File(filepath);
 		while (!f.exists() || f.isDirectory()) {
-			System.out.println("File does not exist or pathname is invalid. Try again: ");
+			System.out.println("File does not exist or path is invalid. Try again: ");
 			filepath = sc.nextLine();
 			f = new File(filepath);
 		}
 		
 		f.renameTo(new File(this.dir+"\\"+f.getName()));
 
+	}
+	
+	public void removeFile() {
+		Scanner sc = new Scanner(System.in);
+		int in;
+		String filepath, reloc;
+		
+		printDR();
+		System.out.println("Enter the name of the file you would like removed from your Data Resource: ");
+		filepath = this.dir + "\\" + sc.nextLine();
+		System.out.println("Would you like to delete(1) or refactor(2): ");
+		in = sc.nextInt();
+		sc.nextLine();
+		if(in == 1) {
+			try {
+				File f = new File(filepath);
+				if(f.delete()) {
+					System.out.println("Your file was successfully deleted");
+				} else {
+					System.out.println("Your file failed to delete, Check if file exists.");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}else if(in == 2) {
+			System.out.println("Where you would like to refactor to: ");
+			reloc = sc.nextLine();
+			try {
+				File f = new File(filepath);
+				if(f.renameTo(new File(reloc+"\\"+f.getName()))) {
+					System.out.println("Your file was successfully refactored");
+				} else {
+					System.out.println("Your file failed to refactor, Check if file exists.");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	//sc.close();	
+	}
+	
+	public void printDR() {
+		String[] fileNames;
+		
+		File f = new File(this.dir);
+		fileNames = f.list();
+		for(String fileName : fileNames) {
+			System.out.println(fileName);
+		}
 	}
 
 }
--- MainMenu.java
+++ MainMenu.java
@@ -1,6 +1,6 @@
 package p1;
 
-import java.io.File;
+//import java.io.File;
 import java.util.Scanner;
 
 public class MainMenu {
@@ -9,29 +9,37 @@ public class MainMenu {
 	public static void main(String[] args) {
 		
 		//load existing list (doesn't actually load anything at the moment)
-		//DataResourceList drl = new DataResourceList();
+		DataResourceList drl = new DataResourceList();
+		
+		DataResource dr = new DataResource();
 		
 		// add try catch in case they enter something stupid
-		System.out.println(
-				"What would you like to do?\n(1) Create a Data Resource.\n(2) Remove a Data Resource.\n(3) Edit an existing Data Resource.\n(4) Exit.");
+		System.out.println("What would you like to do?\n(1) Create a Data Resource.\n(2) Remove a Data Resource.\n(3) Edit an existing Data Resource.\n(4) Exit.");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 
-		while (true) {
+		while (n != 4) {
 			switch (n) {
 			case 1:
-				//Create Data Resource
-				DataResource dr = new DataResource();
-
+				//add file to new/current data resource
+				dr.addFile();
+				if(dr.created == true) {
+					drl.addDataResource(dr);
+				}
 				break;
 			case 2:
-				System.out.println("You picked 2");
+				if(dr.created == true) {
+					dr.removeFile();
+				}else {
+					System.out.print("No Data Resource is avaiab");
+				}
+				
 				break;
 			case 3:
-				System.out.println("You picked 3");
+				System.out.print("you picked 3");
+				
 				break;
 			case 4:
-				//System.out.println("Saving....");
 				System.out.println("Exiting...");
 				System.exit(0);
 				break;
@@ -40,8 +48,10 @@ public class MainMenu {
 				n = sc.nextInt();
 				break;
 			}
+			System.out.println("What would you like to do?\n(1) Add to a Data Resource.\n(2) Remove a Data Resource.\n(3) Edit an existing Data Resource.\n(4) Exit.");
+			n = sc.nextInt();
 		}
-
+		//sc.close();
 	}
 
 }
